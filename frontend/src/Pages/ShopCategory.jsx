1aee22c711e5a4012e97ad4445879453b8890efd
Rajat Kumar <rohitnrajat@gmail.com>
2024-02-03 19:09:49 +0530

    updated multiple files in frontend

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-const ShopCategory = () => {
-  return (
-    <div>
+import React , {useContext} from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import './CSS/ShopCategory.css'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item'
 
+const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext);
+  return (
+    <div className='shop-category'>
+      <img className='shopcategory-banner' src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12 </span>out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          sort by <img src={dropdown_icon}></img>
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_products.map((item,i)=>{
+          if(props.category === item.category){
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} 
+            old_price={item.old_price}/>}
+            else{
+              return null;
+            }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
     </div>
   )
 }
