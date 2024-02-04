f5ff948ff3b235363055f683aa18e88972907c78
Rajat Kumar <rohitnrajat@gmail.com>
2024-02-05 02:44:22 +0530

    updated multiple files again in frontend directory

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from "../Assets/star_icon.png"
+import star_dull_icon from "../Assets/star_dull_icon.png"
+const ProductDisplay = (props) => {
+    const {product} = props;
+  return (
+    <div className='productdisplay'>
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+            <img src={product.image} />
+            <img src={product.image} />
+            <img src={product.image} />
+            <img src={product.image} />
+        </div>
+        <div className="productdisplay-img">
+            <img src={product.image} alt="" className="productdisplay-main-img" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_dull_icon} alt="" />
+            <p>(122)</p>
+        </div>
+        <div className='productdisplay-right-prices'>
+            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            <div className="productdisplay-right-price-now">Now: ${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+            A lightweight , usually knitted , pullover shirt , close-fitting and a round neckline and short sleeves , worn as an undershirt or outer garment .
+        </div>
+        <div className="productdisplay-right-size">
+            <h1>Select Size</h1>
+            <div className="productdisplay-right-size">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+            </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category : </span><span>Women , T-shirt , Crop Top</span></p>
+        <p className='productdisplay-right-category'><span>Tags : </span><span>Modern , Latest</span></p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 const Product = () => {
+  const { all_products } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  // Check if all_product is defined before trying to find the product
+  const product = all_products.find((e) => e.id === Number(productId));
+
   return (
     <div>
-      
+      <Breadcrum product={product} />
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
